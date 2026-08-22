cb18c13dcc5c4727d662cb1bb3db628fab4d3694
tariq <103262733+owen-tariq@users.noreply.github.com>
2026-08-22 19:00:40 -0400

    chore: bump to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.owentariq.emberlink"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.2"
     }
 
     signingConfigs {
